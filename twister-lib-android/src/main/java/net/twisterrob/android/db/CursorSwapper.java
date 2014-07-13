c52cfbebdfb0f4df75b18099f265894942b849d2
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2014-07-13 19:06:36 +0000

    [SVN] r1129 Inventory in /Libraries/twister-lib-android/ [REF] Reorganize classes

---
+++ twister-lib-android/src/main/java/net/twisterrob/android/db/CursorSwapper.java
@@ -0,0 +1,29 @@
+package net.twisterrob.android.db;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+
+public abstract class CursorSwapper implements LoaderCallbacks<Cursor> {
+	protected final Context context;
+	protected final CursorAdapter adapter;
+
+	public CursorSwapper(Context context, CursorAdapter adapter) {
+		this.context = context;
+		this.adapter = adapter;
+	}
+
+	public void onLoaderReset(Loader<Cursor> loader) {
+		updateAdapter(null);
+	}
+
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		updateAdapter(data);
+	}
+
+	protected void updateAdapter(Cursor data) {
+		adapter.swapCursor(data);
+	}
+}
