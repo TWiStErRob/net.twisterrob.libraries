5cde5b8fa6de3407c482a2df241840265769b4fa
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2014-12-13 23:53:37 +0000

    [SVN] r1443 Inventory in /Libraries/twister-lib-android/ [REF] Move listeners to lib-android [REM] Unused fancy snow [FIX] bunch of lint warnings

---
+++ twister-lib-android/src/main/java/net/twisterrob/android/utils/listeners/LoggingOnTouchListener.java
@@ -0,0 +1,25 @@
+package net.twisterrob.android.utils.listeners;
+
+import org.slf4j.*;
+
+import android.annotation.SuppressLint;
+import android.view.*;
+
+@SuppressLint("ClickableViewAccessibility")
+public class LoggingOnTouchListener implements View.OnTouchListener {
+	private static final Logger LOGGER = LoggerFactory.getLogger("ViewOnTouch");
+	private final Logger LOG;
+
+	public LoggingOnTouchListener() {
+		this(LOGGER);
+	}
+
+	public LoggingOnTouchListener(Logger log) {
+		LOG = log;
+	}
+
+	@Override public boolean onTouch(View v, MotionEvent event) {
+		LOG.trace("onTouch({}, {})", v, event);
+		return false;
+	}
+}
