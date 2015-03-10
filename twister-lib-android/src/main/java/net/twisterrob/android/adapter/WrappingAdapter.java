2c800bc22f0d1d4528e0fa9763ed014bb0594ad5
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2015-03-10 00:05:40 +0000

    [SVN] r1486 Inventory in /Libraries/twister-lib-android/ [EXP] List categories and items together with header

---
+++ twister-lib-android/src/main/java/net/twisterrob/android/adapter/WrappingAdapter.java
@@ -0,0 +1,81 @@
+package net.twisterrob.android.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.view.ViewGroup;
+
+public abstract class WrappingAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
+	protected RecyclerView.Adapter<VH> mWrappedAdapter;
+	protected RecyclerView.AdapterDataObserver mDataObserver = new NotifyingObserver();
+
+	protected WrappingAdapter() {
+		// child must call setWrappedAdapter after it's initialization
+	}
+	public WrappingAdapter(@NonNull RecyclerView.Adapter<VH> wrapped) {
+		setWrappedAdapter(wrapped);
+	}
+
+	protected void setWrappedAdapter(@NonNull Adapter<VH> wrapped) {
+		if (mWrappedAdapter != null) {
+			mWrappedAdapter.unregisterAdapterDataObserver(mDataObserver);
+		}
+		this.mWrappedAdapter = wrapped;
+		setHasStableIds(wrapped.hasStableIds());
+		mWrappedAdapter.registerAdapterDataObserver(mDataObserver);
+	}
+
+	@Override public VH onCreateViewHolder(ViewGroup group, int viewType) {
+		return mWrappedAdapter.onCreateViewHolder(group, viewType);
+	}
+	@Override public void onBindViewHolder(VH holder, int position) {
+		mWrappedAdapter.onBindViewHolder(holder, position);
+	}
+	@Override public int getItemCount() {
+		return mWrappedAdapter.getItemCount();
+	}
+	@Override public long getItemId(int position) {
+		return mWrappedAdapter.getItemId(position);
+	}
+	@Override public int getItemViewType(int position) {
+		return mWrappedAdapter.getItemViewType(position);
+	}
+	@Override public void onViewAttachedToWindow(VH holder) {
+		mWrappedAdapter.onViewAttachedToWindow(holder);
+	}
+	@Override public void onViewDetachedFromWindow(VH holder) {
+		mWrappedAdapter.onViewDetachedFromWindow(holder);
+	}
+	@Override public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+		mWrappedAdapter.onDetachedFromRecyclerView(recyclerView);
+	}
+	@Override public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+		mWrappedAdapter.onAttachedToRecyclerView(recyclerView);
+	}
+	@Override public void onViewRecycled(VH holder) {
+		mWrappedAdapter.onViewRecycled(holder);
+	}
+
+	protected class NotifyingObserver extends RecyclerView.AdapterDataObserver {
+		@Override public void onChanged() {
+			notifyDataSetChanged();
+		}
+
+		@Override public void onItemRangeChanged(int positionStart, int itemCount) {
+			notifyItemRangeChanged(positionStart, itemCount);
+		}
+
+		@Override public void onItemRangeInserted(int positionStart, int itemCount) {
+			notifyItemRangeInserted(positionStart, itemCount);
+		}
+
+		@Override public void onItemRangeRemoved(int positionStart, int itemCount) {
+			notifyItemRangeRemoved(positionStart, itemCount);
+		}
+
+		@Override public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+			// TODO: No notifyItemRangeMoved method?
+			notifyItemRangeChanged(fromPosition, toPosition + itemCount);
+		}
+	}
+}
