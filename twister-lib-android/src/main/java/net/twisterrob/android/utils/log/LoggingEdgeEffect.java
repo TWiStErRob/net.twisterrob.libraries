35ae7b7da6f526cb305003f80799170e3d43a4c9
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2016-05-31 11:46:34 +0000

    [SVN] r1883 Inventory in /Libraries/twister-lib-android/ [FIX] IDEA 2016.2 doesn't find Java files in buildSrc [FIX] Few inspection warnings (e.g. broad scope, empty method) [REF] Move LoggingEdgeEffect to logging package by extracting helper [FIX] Add specialization for IOTools.ignorantClose for Cursor

---
+++ twister-lib-android/src/main/java/net/twisterrob/android/utils/log/LoggingEdgeEffect.java
@@ -0,0 +1,70 @@
+package net.twisterrob.android.utils.log;
+
+import org.slf4j.*;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.v4.widget.EdgeEffectWrapper;
+
+import net.twisterrob.java.annotations.DebugHelper;
+
+/**
+ * Register in {@link android.app.Application#onCreate} via
+ * <code>
+ * EdgeEffectWrapper.install(LoggingEdgeEffect.class);
+ * </code>
+ *
+ * @see EdgeEffectWrapper#install(Class)
+ */
+@DebugHelper
+public class LoggingEdgeEffect extends EdgeEffectWrapper {
+	private static final Logger LOG = LoggerFactory.getLogger(LoggingEdgeEffect.class);
+
+	public LoggingEdgeEffect(Object effect) {
+		super(effect);
+	}
+
+	@Override public Object newEdgeEffect(Context context) {
+		Object result = super.newEdgeEffect(context);
+		LOG.trace("newEdgeEffect({}): {}", context, result);
+		return result;
+	}
+	@Override public void setSize(Object edgeEffect, int width, int height) {
+		LOG.trace("setSize({}, {}, {})", edgeEffect, width, height);
+		super.setSize(edgeEffect, width, height);
+	}
+	@Override public boolean isFinished(Object edgeEffect) {
+		boolean result = super.isFinished(edgeEffect);
+		LOG.trace("isFinished({}): {}", edgeEffect, result);
+		return result;
+	}
+	@Override public void finish(Object edgeEffect) {
+		LOG.trace("finish({})", edgeEffect);
+		super.finish(edgeEffect);
+	}
+	@Override public boolean onPull(Object edgeEffect, float deltaDistance) {
+		boolean result = super.onPull(edgeEffect, deltaDistance);
+		LOG.trace("onPull({}, {}): {}", edgeEffect, deltaDistance, result);
+		return result;
+	}
+	@Override public boolean onPull(Object edgeEffect, float deltaDistance, float displacement) {
+		boolean result = super.onPull(edgeEffect, deltaDistance, displacement);
+		LOG.trace("onPull({}, {}, {}): {}", edgeEffect, deltaDistance, result);
+		return result;
+	}
+	@Override public boolean onRelease(Object edgeEffect) {
+		boolean result = super.onRelease(edgeEffect);
+		LOG.trace("onRelease({}): {}", edgeEffect, result);
+		return result;
+	}
+	@Override public boolean onAbsorb(Object edgeEffect, int velocity) {
+		boolean result = super.onAbsorb(edgeEffect, velocity);
+		LOG.trace("onAbsorb({}, {}): {}", edgeEffect, velocity, result);
+		return result;
+	}
+	@Override public boolean draw(Object edgeEffect, Canvas canvas) {
+		boolean result = super.draw(edgeEffect, canvas);
+		LOG.trace("draw({}, {}): {}", edgeEffect, canvas, result);
+		return result;
+	}
+}
