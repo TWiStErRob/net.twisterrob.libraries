322a093cea2ee0b1f8ddfa7d688b7afc9c808c69
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-01 12:54:07 +0000

    Remove unnecessary annotations Gradle 7.6 doesn't incubate those any more.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,13 +7,11 @@ pluginManagement {
 }
 
 includeBuild("twister-lib-java") {
-	@Suppress("UnstableApiUsage")
 	name = "twister-libs-java"
 	apply(from = projectDir.resolve("settings.substitutions.gradle"), to = this)
 }
 
 includeBuild("twister-lib-android") {
-	@Suppress("UnstableApiUsage")
 	name = "twister-libs-android"
 	apply(from = projectDir.resolve("settings.substitutions.gradle"), to = this)
 }
