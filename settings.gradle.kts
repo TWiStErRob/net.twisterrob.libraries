ef069c6f6f9af32305a770e4d37010709a08b7ee
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-01 12:53:32 +0000

    Include plugins in root build too

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,17 @@
+pluginManagement {
+	includeBuild("gradle/plugins")
+	repositories {
+		google()
+		mavenCentral()
+	}
+}
+
 includeBuild("twister-lib-java") {
 	@Suppress("UnstableApiUsage")
 	name = "twister-libs-java"
 	apply(from = projectDir.resolve("settings.substitutions.gradle"), to = this)
 }
+
 includeBuild("twister-lib-android") {
 	@Suppress("UnstableApiUsage")
 	name = "twister-libs-android"
