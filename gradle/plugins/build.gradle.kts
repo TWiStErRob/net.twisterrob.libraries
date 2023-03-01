0477abd92833afbc94779a31e17ba67475e78a51
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-01 11:17:20 +0000

    Add empty plugins project

---
+++ gradle/plugins/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	`kotlin-dsl`
+}
+
+dependencies {
+	compileOnly(gradleApi())
+	implementation(libs.kotlin.gradle)
+	implementation(libs.kotlin.detekt)
+	implementation(libs.android.gradle)
+	implementation(libs.twisterrob.quality)
+	implementation(libs.twisterrob.android)
+	// TODEL https://github.com/gradle/gradle/issues/15383
+	implementation(files(libs::class.java.protectionDomain.codeSource.location))
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+	kotlinOptions {
+		allWarningsAsErrors = true
+	}
+}
+
+tasks.named("pluginDescriptors").configure {
+	finalizedBy("validatePlugins")
+}
+
+tasks.withType<ValidatePlugins>().configureEach {
+	ignoreFailures.set(false)
+	failOnWarning.set(true)
+	enableStricterValidation.set(true)
+}
