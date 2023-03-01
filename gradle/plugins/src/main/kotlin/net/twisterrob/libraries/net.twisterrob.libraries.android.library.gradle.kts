6d6d45f8bc55bb37653ef67d19a8aeaa26f6abdf
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-01 13:28:13 +0000

    Introduce convention for testing libraries

--- gradle/plugins/src/main/kotlin/net/twisterrob/libraries/net.twisterrob.libraries.android.library.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/libraries/net.twisterrob.libraries.android.library.gradle.kts
@@ -2,4 +2,5 @@ plugins {
 	id("net.twisterrob.libraries.build.publishing")
 	id("net.twisterrob.libraries.build.android.library")
 	id("net.twisterrob.libraries.build.android.base")
+	id("net.twisterrob.libraries.build.android.test")
 }
