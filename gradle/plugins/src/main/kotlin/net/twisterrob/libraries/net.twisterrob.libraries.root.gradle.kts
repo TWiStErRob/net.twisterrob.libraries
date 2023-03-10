00c6b5d9bfb2d1d4b1aa3c807e6970d43e948c88
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-10 12:22:47 +0000

    Add allDependencies task

--- gradle/plugins/src/main/kotlin/net/twisterrob/libraries/net.twisterrob.libraries.root.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/libraries/net.twisterrob.libraries.root.gradle.kts
@@ -11,3 +11,18 @@ tasks.register("cleanFull") {
 	dependsOn(subprojects.map { "${it.path}:clean" })
 	dependsOn(tasks.named("clean"))
 }
+
+// To get gradle/dependency-locks run `gradlew :allDependencies --write-locks`.
+tasks.register<Task>("allDependencies") {
+	val projects = project.allprojects.sortedBy { it.path }
+	val projectPaths = "Printing dependencies for modules:\n" +
+		projects.joinToString("\n") { " * ${it}" }
+	doFirst {
+		println(projectPaths)
+	}
+	val dependenciesTasks = projects.map { it.tasks.named("dependencies") }
+	// Builds a dependency chain: 1 <- 2 <- 3 <- 4, so when executed they're in order.
+	dependenciesTasks.reduce { acc, task -> task.apply { get().dependsOn(acc) } }
+	// Use finalizedBy instead of dependsOn to make sure this task executes first.
+	this@register.finalizedBy(dependenciesTasks)
+}
