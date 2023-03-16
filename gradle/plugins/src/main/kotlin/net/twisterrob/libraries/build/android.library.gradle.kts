3b043db7d44d7add26e3355603246d0a77b9cc4f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-16 16:42:39 +0000

    Fix androidTest duplicate classes

--- gradle/plugins/src/main/kotlin/net/twisterrob/libraries/build/android.library.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/libraries/build/android.library.gradle.kts
@@ -1,5 +1,7 @@
 package net.twisterrob.libraries.build
 
+import net.twisterrob.libraries.build.dsl.libs
+
 plugins {
 	id("net.twisterrob.gradle.plugin.android-library")
 }
@@ -7,5 +9,14 @@ plugins {
 android {
 	twisterrob {
 		isDecorateBuildConfig = false
+	}
+}
+
+dependencies {
+	// Lock in the version of Kotlin used so that the transitive dependencies are consistently upgraded.
+	// https://kotlinlang.org/docs/whatsnew18.html#usage-of-the-latest-kotlin-stdlib-version-in-transitive-dependencies
+	implementation(platform(libs.kotlin.bom))
+	constraints {
+		implementation(libs.androidx.lifecycleViewModel)
 	}
 }
