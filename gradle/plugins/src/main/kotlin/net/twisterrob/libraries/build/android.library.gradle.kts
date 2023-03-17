9212ecef723f1161622220a82dbe29464e2a2926
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-17 17:08:13 +0000

    Move Kotlin BOM to platform and remove leftovers

--- gradle/plugins/src/main/kotlin/net/twisterrob/libraries/build/android.library.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/libraries/build/android.library.gradle.kts
@@ -1,7 +1,5 @@
 package net.twisterrob.libraries.build
 
-import net.twisterrob.libraries.build.dsl.libs
-
 plugins {
 	id("net.twisterrob.gradle.plugin.android-library")
 }
@@ -9,14 +7,5 @@ plugins {
 android {
 	twisterrob {
 		isDecorateBuildConfig = false
-	}
-}
-
-dependencies {
-	// Lock in the version of Kotlin used so that the transitive dependencies are consistently upgraded.
-	// https://kotlinlang.org/docs/whatsnew18.html#usage-of-the-latest-kotlin-stdlib-version-in-transitive-dependencies
-	implementation(platform(libs.kotlin.bom))
-	constraints {
-		implementation(libs.androidx.lifecycleViewModel)
 	}
 }
