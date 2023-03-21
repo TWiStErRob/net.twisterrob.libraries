298a43033ddf71b8affc43cf15887b627d02c6c9
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-21 22:02:25 +0000

    Remove unused import

--- twister-lib-android/monolith/src/main/java/net/twisterrob/android/db/DatabaseOpenHelper.java
+++ twister-lib-android/monolith/src/main/java/net/twisterrob/android/db/DatabaseOpenHelper.java
@@ -16,8 +16,6 @@ import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.os.Build.*;
 import android.os.Environment;
 
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-
 import androidx.annotation.*;
 
 import net.twisterrob.android.utils.tools.*;
