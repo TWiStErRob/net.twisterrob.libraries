11e7f4010d0bbefc7f5199af7ae937495ca8c40f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-05-23 12:01:39 +0100

    Add logging to ViewModel

---
+++ twister-lib-android/logging/src/main/java/net/twisterrob/android/utils/log/LoggingViewModel.java
@@ -0,0 +1,40 @@
+package net.twisterrob.android.utils.log;
+
+import java.io.Closeable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+
+import net.twisterrob.android.utils.log.LoggingDebugProvider.LoggingHelper;
+import net.twisterrob.android.utils.tools.StringerTools;
+
+public class LoggingViewModel extends ViewModel {
+	private static final Logger LOG = LoggerFactory.getLogger("ViewModel");
+
+	public LoggingViewModel() {
+		super();
+		log("<ctor>");
+	}
+
+	public LoggingViewModel(@NonNull Closeable... closeables) {
+		super(closeables);
+		log("<ctor>", (Object)closeables);
+	}
+
+	@Override public void addCloseable(@NonNull Closeable closeable) {
+		log("addCloseable", closeable);
+		super.addCloseable(closeable);
+	}
+
+	@Override protected void onCleared() {
+		log("onCleared");
+		super.onCleared();
+	}
+
+	private void log(@NonNull String method, @NonNull Object... args) {
+		LoggingHelper.log(LOG, StringerTools.toNameString(this), method, null, args);
+	}
+}
