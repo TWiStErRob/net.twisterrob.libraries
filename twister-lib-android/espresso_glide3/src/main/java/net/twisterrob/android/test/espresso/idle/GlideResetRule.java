1802b59b7e6b662d056dcda95d7e935c86b78cd6
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-07-26 10:08:10 +0100

    Make context injectable in GlideResetRule

--- twister-lib-android/espresso_glide3/src/main/java/net/twisterrob/android/test/espresso/idle/GlideResetRule.java
+++ twister-lib-android/espresso_glide3/src/main/java/net/twisterrob/android/test/espresso/idle/GlideResetRule.java
@@ -2,14 +2,27 @@ package net.twisterrob.android.test.espresso.idle;
 
 import org.junit.rules.ExternalResource;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.test.core.app.ApplicationProvider;
 
 public class GlideResetRule extends ExternalResource {
+	private final @NonNull Context appContext;
+
+	public GlideResetRule() {
+		this(ApplicationProvider.getApplicationContext());
+	}
+
+	public GlideResetRule(@NonNull Context appContext) {
+		this.appContext = appContext;
+	}
+
 	@Override protected void before() {
-		GlideResetter.resetGlide(InstrumentationRegistry.getInstrumentation().getTargetContext());
+		GlideResetter.resetGlide(appContext);
 	}
 
 	@Override protected void after() {
-		GlideResetter.resetGlide(InstrumentationRegistry.getInstrumentation().getTargetContext());
+		GlideResetter.resetGlide(appContext);
 	}
 }
