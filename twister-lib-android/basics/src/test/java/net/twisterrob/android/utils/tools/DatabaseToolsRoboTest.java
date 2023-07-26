10ded8438bc7fa141ffe095292dfa5eb33d083ad
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-07-26 22:21:12 +0100

    CI has a different SQLite version

--- twister-lib-android/basics/src/test/java/net/twisterrob/android/utils/tools/DatabaseToolsRoboTest.java
+++ twister-lib-android/basics/src/test/java/net/twisterrob/android/utils/tools/DatabaseToolsRoboTest.java
@@ -3,6 +3,7 @@ package net.twisterrob.android.utils.tools;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -30,7 +31,16 @@ public class DatabaseToolsRoboTest {
 
 		String result = DatabaseTools.dbToString(db);
 
-		assertEquals("v0(1)::3.8.7@" + dbFile.getAbsolutePath(), result);
+		// Even though Robolectric has a hardcoded version:
+		// https://github.com/robolectric/robolectric/blob/robolectric-4.10.3/dependencies.gradle#L39
+		// On CI the version is still different.
+		if (SystemUtils.IS_OS_WINDOWS) {
+			assertEquals("v0(1)::3.8.7@" + dbFile.getAbsolutePath(), result);
+		} else {
+			// The source says 3.8.7: https://bitbucket.org/almworks/sqlite4java/src/master/
+			// Not sure where this other version comes from.
+			assertEquals("v0(1)::3.32.2@" + dbFile.getAbsolutePath(), result);
+		}
 	}
 
 	@Test
