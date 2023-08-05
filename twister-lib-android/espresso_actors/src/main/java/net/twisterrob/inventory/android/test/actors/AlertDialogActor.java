6c28eb2a031023d0ade5c688ca7d0a9662eefa9a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-08-05 02:27:12 +0100

    Add dialog title assertion

--- twister-lib-android/espresso_actors/src/main/java/net/twisterrob/inventory/android/test/actors/AlertDialogActor.java
+++ twister-lib-android/espresso_actors/src/main/java/net/twisterrob/inventory/android/test/actors/AlertDialogActor.java
@@ -4,6 +4,8 @@ import org.hamcrest.Matcher;
 
 import static org.hamcrest.Matchers.*;
 
+import android.view.View;
+
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
@@ -11,6 +13,10 @@ import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static net.twisterrob.android.test.espresso.DialogMatchers.*;
 
 public class AlertDialogActor {
+	protected static void assertDialogTitle(Matcher<View> matcher) {
+		onView(isDialogTitle())
+				.check(matches(matcher));
+	}
 
 	protected final void assertDialogMessage(Matcher<View> matcher) {
 		onView(withText(matchesPattern("%\\d"))).check(doesNotExist());
