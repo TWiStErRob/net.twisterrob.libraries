2b97e0c2cf3f48a10b9dcbda749cb19785b6316c
Róbert Papp <papp.robert.s@gmail.com>
2023-08-05 03:23:59 +0100

    Generalize AlertDialogActor assertions (#67)

--- twister-lib-android/espresso_actors/src/main/java/net/twisterrob/inventory/android/test/actors/AlertDialogActor.java
+++ twister-lib-android/espresso_actors/src/main/java/net/twisterrob/inventory/android/test/actors/AlertDialogActor.java
@@ -4,6 +4,8 @@ import org.hamcrest.Matcher;
 
 import static org.hamcrest.Matchers.*;
 
+import android.view.View;
+
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
@@ -11,29 +13,39 @@ import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static net.twisterrob.android.test.espresso.DialogMatchers.*;
 
 public class AlertDialogActor {
-	protected final void assertDialogMessage(Matcher<String> matcher) {
+	protected static void assertDialogTitle(Matcher<View> matcher) {
+		onView(isDialogTitle())
+				.check(matches(matcher));
+	}
+
+	protected final void assertDialogMessage(Matcher<View> matcher) {
 		onView(withText(matchesPattern("%\\d"))).check(doesNotExist());
 		onView(isDialogMessage())
 				.check(matches(isCompletelyDisplayed()))
-				.check(matches(withText(matcher)))
+				.check(matches(matcher))
 		;
 	}
-	protected final void assertToastMessage(Matcher<String> matcher) {
+
+	protected final void assertToastMessage(Matcher<View> matcher) {
 		onView(isDialogMessage())
 				.inRoot(isToast())
 				.check(matches(isCompletelyDisplayed()))
-				.check(matches(withText(matcher)))
+				.check(matches(matcher))
 		;
 	}
+
 	public void assertNoToastDisplayed() {
 		assertNoToastIsDisplayed();
 	}
+
 	public void assertNotDisplayed() {
 		assertNoDialogIsDisplayed();
 	}
+
 	public void assertDisplayed() {
 		assertDialogIsDisplayed();
 	}
+
 	protected void dismissWithNeutral() {
 		clickNeutralInDialog();
 		assertNoDialogIsDisplayed();
