8162a217649a1cfd3c8da6b720d1af2809f28f02
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-08-05 02:19:23 +0100

    Generalize AlertDialogActor for greater reusability

--- twister-lib-android/espresso_actors/src/main/java/net/twisterrob/inventory/android/test/actors/AlertDialogActor.java
+++ twister-lib-android/espresso_actors/src/main/java/net/twisterrob/inventory/android/test/actors/AlertDialogActor.java
@@ -11,29 +11,35 @@ import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static net.twisterrob.android.test.espresso.DialogMatchers.*;
 
 public class AlertDialogActor {
-	protected final void assertDialogMessage(Matcher<String> matcher) {
+
+	protected final void assertDialogMessage(Matcher<View> matcher) {
 		onView(withText(matchesPattern("%\\d"))).check(doesNotExist());
 		onView(isDialogMessage())
 				.check(matches(isCompletelyDisplayed()))
-				.check(matches(withText(matcher)))
+				.check(matches(matcher))
 		;
 	}
-	protected final void assertToastMessage(Matcher<String> matcher) {
+
+	protected final void assertToastMessage(Matcher<View> matcher) {
 		onView(isDialogMessage())
 				.inRoot(isToast())
 				.check(matches(isCompletelyDisplayed()))
-				.check(matches(withText(matcher)))
+				.check(matches(matcher))
 		;
 	}
+
 	public void assertNoToastDisplayed() {
 		assertNoToastIsDisplayed();
 	}
+
 	public void assertNotDisplayed() {
 		assertNoDialogIsDisplayed();
 	}
+
 	public void assertDisplayed() {
 		assertDialogIsDisplayed();
 	}
+
 	protected void dismissWithNeutral() {
 		clickNeutralInDialog();
 		assertNoDialogIsDisplayed();
