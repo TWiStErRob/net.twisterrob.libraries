139ff66f6b2989ab49fdcccb6708fa8f9ab28e23
Róbert Papp <papp.robert.s@gmail.com>
2023-09-15 23:19:46 +0100

    Update isCI.kt

--- gradle/plugins/src/main/kotlin/net/twisterrob/libraries/build/dsl/isCI.kt
+++ gradle/plugins/src/main/kotlin/net/twisterrob/libraries/build/dsl/isCI.kt
@@ -3,5 +3,6 @@ package net.twisterrob.libraries.build.dsl
 /**
  * See [GitHub Actions: Default environment variables](https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables).
  */
+@Suppress("MaxLineLength")
 public val isCI: Boolean
 	get() = System.getenv("GITHUB_ACTIONS") == "true"
