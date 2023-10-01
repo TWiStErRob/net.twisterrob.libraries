dd7750e0f93e7931357149a286151f0e2dc41965
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-10-01 14:28:00 +0100

    Fix ClassCastException for .load(resId) when logging is enabled

--- twister-lib-android/espresso_glide4/src/main/java/com/bumptech/glide/load/engine/EngineKey_toString.kt
+++ twister-lib-android/espresso_glide4/src/main/java/com/bumptech/glide/load/engine/EngineKey_toString.kt
@@ -7,7 +7,7 @@ import net.twisterrob.java.utils.ReflectionTools
 internal fun EngineKey.toStringHack(): String =
 	"${model}[${width}x${height}]"
 
-private val EngineKey.model: String
+private val EngineKey.model: Any?
 	get() = ReflectionTools.get(this, "model")
 
 private val EngineKey.height: Int
