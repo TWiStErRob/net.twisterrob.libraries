87254149a29f41c68410fa81141113e0f83ae9f7
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-10-01 13:53:26 +0100

    Suppress warning in Glide 4 (see Glide.getAnnotationGeneratedGlideModules)

---
+++ twister-lib-android/espresso_glide4/src/androidTest/kotlin/net/twisterrob/android/test/espresso/idle/EspressoGlideTestGlideModule.kt
@@ -0,0 +1,7 @@
+package net.twisterrob.android.test.espresso.idle
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class EspressoGlideTestGlideModule : AppGlideModule()
