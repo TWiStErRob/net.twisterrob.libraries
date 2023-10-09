3f0ed75655fbfafbf46243a60a2f30cfe6271d54
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-10-09 12:42:15 +0100

    Fix visibility modifier

--- twister-lib-android/espresso_glide4/src/main/java/com/bumptech/glide/Glide_engine.kt
+++ twister-lib-android/espresso_glide4/src/main/java/com/bumptech/glide/Glide_engine.kt
@@ -5,7 +5,7 @@ package com.bumptech.glide
 
 import com.bumptech.glide.load.engine.Engine
 
-val Glide.engine: Engine
+internal val Glide.engine: Engine
 	get() = Glide::class.java
 		.getDeclaredField("engine")
 		.apply { isAccessible = true }
