87e9c7a2b04c80350042f812695e0faad9a341ce
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-01-25 09:31:21 +0000

    Add aggregate coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	id("net.twisterrob.libraries.root")
 	id("net.twisterrob.libraries.build.dependencyAnalysis")
+	id("net.twisterrob.libraries.build.instrumentedTestReport")
 }
 
 // :plugins:check is not automatically invoked when doing `gradlew check`. Help Gradle discover it.
