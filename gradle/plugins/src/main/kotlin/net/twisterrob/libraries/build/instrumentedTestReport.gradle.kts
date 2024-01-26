705a8072d27584641215232033a63a8439394174
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-01-26 22:55:36 +0000

    Fix reporting

--- gradle/plugins/src/main/kotlin/net/twisterrob/libraries/build/instrumentedTestReport.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/libraries/build/instrumentedTestReport.gradle.kts
@@ -1,5 +1,7 @@
 package net.twisterrob.libraries.build
 
+import com.android.build.gradle.internal.tasks.AndroidReportTask
+
 // TODEL https://issuetracker.google.com/issues/222730176
 // This makes sure to pick up all subprojects not just direct children.
 // com.android.build.gradle.internal.plugins.ReportingPlugin reads the subprojects in afterEvaluate,
@@ -8,3 +10,13 @@ subprojects.forEach { evaluationDependsOn(it.path) } // evaluationDependsOnSubpr
 
 // https://developer.android.com/studio/test/command-line#multi-module-reports-instrumented-tests
 apply(plugin = "android-reporting")
+
+afterEvaluate {
+	tasks.named<AndroidReportTask>("mergeAndroidReports").configure {
+		mustRunAfter(dependsOn)
+		setDependsOn(emptyList<Task>())
+		doFirst {
+			resultsDirectories.removeIf { !it.exists() }
+		}
+	}
+}
