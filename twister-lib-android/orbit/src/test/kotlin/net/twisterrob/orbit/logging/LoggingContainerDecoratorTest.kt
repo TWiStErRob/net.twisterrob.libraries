a4b3b411be3591cc581fec24a338ff04c9157da0
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-04-05 20:46:41 +0100

    Test sub-intents

--- twister-lib-android/orbit/src/test/kotlin/net/twisterrob/orbit/logging/LoggingContainerDecoratorTest.kt
+++ twister-lib-android/orbit/src/test/kotlin/net/twisterrob/orbit/logging/LoggingContainerDecoratorTest.kt
@@ -7,22 +7,24 @@ import net.twisterrob.mockito.captureSingle
 import net.twisterrob.orbit.logging.LoggingContainerDecorator.OrbitEvents
 import net.twisterrob.orbit.logging.LoggingContainerDecoratorTest.TestContainerHost.Companion.FQCN
 import net.twisterrob.orbit.logging.LoggingContainerDecoratorTest.TestEffect.TestEffect1
+import net.twisterrob.orbit.logging.LoggingContainerDecoratorTest.TestEffect.TestEffect2
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertSame
 import org.junit.Test
+import org.mockito.Mockito.mockingDetails
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.inOrder
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoInteractions
-import org.mockito.kotlin.verifyNoMoreInteractions
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.annotation.OrbitExperimental
 import org.orbitmvi.orbit.container
 import org.orbitmvi.orbit.syntax.simple.blockingIntent
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.syntax.simple.subIntent
 import org.orbitmvi.orbit.test.test
 
 /**
@@ -122,9 +124,72 @@ class LoggingContainerDecoratorTest {
 		}
 	}
 
+	@Test
+	fun testSubIntent() = runTest {
+		val mockEvents: OrbitEvents<TestState, TestEffect> = mock()
+		TestContainerHost(backgroundScope, mockEvents).test(this) {
+			expectInitialState()
+			verifyNoInteractions(mockEvents)
+
+			containerHost.testSubIntent()
+			expectSideEffect(TestEffect1)
+			expectSideEffect(TestEffect2)
+
+			val events = listOf(
+				"intentStarted" to "${FQCN}\$testSubIntent\$1",
+				"intentStarted" to "${FQCN}\$testSubIntent\$1\$1",
+				"sideEffect" to TestEffect1::class.java.name,
+				"intentFinished" to "${FQCN}\$testSubIntent\$1\$1",
+				"intentStarted" to "${FQCN}\$testSubIntent\$1\$2",
+				"sideEffect" to TestEffect2::class.java.name,
+				"intentFinished" to "${FQCN}\$testSubIntent\$1\$2",
+				"intentFinished" to "${FQCN}\$testSubIntent\$1",
+			)
+			val calls = mockingDetails(mockEvents)
+				.invocations
+				.map { it.method.name to it.arguments.single() }
+			assertEquals(events, calls.map { it.first to it.second::class.java.name })
+			assertSame(calls.first().second, calls.last().second)
+			assertSame(calls[1].second, calls[3].second)
+			assertSame(calls[4].second, calls[6].second)
+		}
+	}
+
+	@Test
+	fun testSubIntentNested() = runTest {
+		val mockEvents: OrbitEvents<TestState, TestEffect> = mock()
+		TestContainerHost(backgroundScope, mockEvents).test(this) {
+			expectInitialState()
+			verifyNoInteractions(mockEvents)
+
+			containerHost.testSubIntentNested()
+			expectSideEffect(TestEffect1)
+			expectSideEffect(TestEffect2)
+
+			val events = listOf(
+				"intentStarted" to "${FQCN}\$testSubIntentNested\$1",
+				"intentStarted" to "${FQCN}\$testSubIntentNested\$1\$1",
+				"sideEffect" to TestEffect1::class.java.name,
+				"intentStarted" to "${FQCN}\$testSubIntentNested\$1\$1\$1",
+				"sideEffect" to TestEffect2::class.java.name,
+				"intentFinished" to "${FQCN}\$testSubIntentNested\$1\$1\$1",
+				"intentFinished" to "${FQCN}\$testSubIntentNested\$1\$1",
+				"intentFinished" to "${FQCN}\$testSubIntentNested\$1",
+			)
+			val calls = mockingDetails(mockEvents)
+				.invocations
+				.map { it.method.name to it.arguments.single() }
+			assertEquals(events, calls.map { it.first to it.second::class.java.name })
+			assertSame(calls.first().second, calls.last().second)
+			assertSame(calls[1].second, calls[6].second)
+			assertSame(calls[3].second, calls[5].second)
+		}
+	}
+
 	private data class TestState(val value: Int)
 	private sealed interface TestEffect {
 		data object TestEffect1 : TestEffect
+		data object TestEffect2 : TestEffect
 	}
 
 	private class TestContainerHost(
@@ -152,6 +217,30 @@ class LoggingContainerDecoratorTest {
 			blockingIntent {
 				reduce {
 					state.copy(value = state.value + 1)
+				}
+			}
+		}
+
+		@OptIn(OrbitExperimental::class) 
+		fun testSubIntent() {
+			intent {
+				subIntent {
+					postSideEffect(TestEffect1)
+				}
+				subIntent {
+					postSideEffect(TestEffect2)
+				}
+			}
+		}
+
+		@OptIn(OrbitExperimental::class)
+		fun testSubIntentNested() {
+			intent {
+				subIntent {
+					postSideEffect(TestEffect1)
+					subIntent {
+						postSideEffect(TestEffect2)
+					}
 				}
 			}
 		}
