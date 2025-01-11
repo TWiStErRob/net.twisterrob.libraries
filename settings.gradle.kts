72897abe759e5e0df564f5da59bfae94d6ba35a3
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-01-11 13:58:13 +0000

    Remove nagging

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-import net.twisterrob.gradle.doNotNagAbout
-
 rootProject.name = "net-twisterrob-libraries"
 
 // TODO enable when https://issuetracker.google.com/issues/300617088
@@ -89,14 +87,3 @@ fun Settings.includeAndroidWithTestHelpers(modulePath: String) {
 	val testHelpersModule = project(testHelpersModulePath)
 	testHelpersModule.projectDir = project(modulePath).projectDir.resolve("test_helpers")
 }
-
-val gradleVersion = GradleVersion.current().version
-
-// TODEL https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1305
-doNotNagAbout(
-	"The ProjectDependency.getDependencyProject() method has been deprecated. " +
-			"This is scheduled to be removed in Gradle 9.0. " +
-			"Consult the upgrading guide for further information: " +
-			"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#deprecate_get_dependency_project",
-	"at com.autonomousapps.internal.utils.GradleStringsKt.toIdentifier(gradleStrings.kt:203)",
-)
